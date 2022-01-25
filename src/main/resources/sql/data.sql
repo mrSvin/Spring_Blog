64a38e0d1970719832df55e41386407c4530cea4
VasilievAV <alva@sespel.com>
2022-01-25 13:25:35 +0300

    Commit

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -13,7 +13,7 @@
 -- INSERT INTO `skillbox_blog`.`tag2post` (`post_id`, `tag_id`) VALUES ('2', '2');
 -- INSERT INTO `skillbox_blog`.`tag2post` (`post_id`, `tag_id`) VALUES ('3', '3');
 -- INSERT INTO `skillbox_blog`.`tag2post` (`post_id`, `tag_id`) VALUES ('4', '4');
--- INSERT INTO `skillbox_blog`.`tag2post` (`post_id`, `tag_id`) VALUES ('5', '5');
+-- INSERT INTO `skillbox_blog`.`tag2post` (`post_id`, `tag_id`) VALUES ('5', '4');
 
 -- INSERT INTO `skillbox_blog`.`posts` (`is_active`, `moderation_status`, `moderator_id`, `text`, `time`, `title`, `view_count`, `user_id`) VALUES ('1', 'NEW', '1', 'Hello World!', '14.09.21', 'Test', '1', '1');
 -- INSERT INTO `skillbox_blog`.`posts` (`is_active`, `moderation_status`, `moderator_id`, `text`, `time`, `title`, `view_count`, `user_id`) VALUES ('0', 'ACCEPTED', '2', 'Test 2!', '12.01.22', 'Test 2', '2', '2');
@@ -25,7 +25,7 @@
 -- INSERT INTO `skillbox_blog`.`post_votes` (`time`, `value`, `post_id`, `user_id`) VALUES ('12.01.22', '24', '2', '2');
 -- INSERT INTO `skillbox_blog`.`post_votes` (`time`, `value`, `post_id`, `user_id`) VALUES ('12.01.22', '34', '3', '4');
 -- INSERT INTO `skillbox_blog`.`post_votes` (`time`, `value`, `post_id`, `user_id`) VALUES ('12.01.22', '45', '4', '4');
--- INSERT INTO `skillbox_blog`.`post_votes` (`time`, `value`, `post_id`, `user_id`) VALUES ('12.01.22', '7', '5', '4');
+-- INSERT INTO `skillbox_blog`.`post_votes` (`time`, `value`, `post_id`, `user_id`) VALUES ('12.01.22', '-7', '5', '4');
 
 -- INSERT INTO `skillbox_blog`.`post_comments` (`parent_id`, `text`, `time`, `user_id`, `post_id`) VALUES ('1', 'test comment!', '14.09.21', '1', '1');
 -- INSERT INTO `skillbox_blog`.`post_comments` (`parent_id`, `text`, `time`, `user_id`, `post_id`) VALUES ('2', 'test comment2!', '12.01.22', '3', '2');
@@ -33,11 +33,11 @@
 -- INSERT INTO `skillbox_blog`.`post_comments` (`parent_id`, `text`, `time`, `user_id`, `post_id`) VALUES ('2', 'test comment4!', '12.01.22', '3', '4');
 -- INSERT INTO `skillbox_blog`.`post_comments` (`parent_id`, `text`, `time`, `user_id`, `post_id`) VALUES ('2', 'test comment5!', '12.01.22', '3', '4');
 
--- INSERT INTO `skillbox_blog`.`global_settings` (`code`, `name`, `value`) VALUES ('1111', 'test', '1');
--- INSERT INTO `skillbox_blog`.`global_settings` (`code`, `name`, `value`) VALUES ('2222', 'test2', '2');
--- INSERT INTO `skillbox_blog`.`global_settings` (`code`, `name`, `value`) VALUES ('3333', 'test3', '3');
--- INSERT INTO `skillbox_blog`.`global_settings` (`code`, `name`, `value`) VALUES ('4444', 'test4', '4');
--- INSERT INTO `skillbox_blog`.`global_settings` (`code`, `name`, `value`) VALUES ('5555', 'test5', '5');
+-- INSERT INTO `skillbox_blog`.`global_settings` (`code`, `name`, `value`) VALUES ('MULTIUSER_MODE', 'Многопользовательский режим ', 'YES');
+-- INSERT INTO `skillbox_blog`.`global_settings` (`code`, `name`, `value`) VALUES ('POST_PREMODERATION', 'Премодерация постов', 'YES');
+-- INSERT INTO `skillbox_blog`.`global_settings` (`code`, `name`, `value`) VALUES ('STATISTICS_IS_PUBLIC', 'Показывать всем статистику блога', 'NO');
+-- INSERT INTO `skillbox_blog`.`global_settings` (`code`, `name`, `value`) VALUES ('MULTIUSER_MODE', 'Многопользовательский режим ', 'NO');
+-- INSERT INTO `skillbox_blog`.`global_settings` (`code`, `name`, `value`) VALUES ('POST_PREMODERATION', 'Премодерация постов', 'NO');
 
 -- INSERT INTO `skillbox_blog`.`captcha_codes` (`code`, `secret_code`, `time`) VALUES ('321', '1111', '2014.09.21');
 -- INSERT INTO `skillbox_blog`.`captcha_codes` (`code`, `secret_code`, `time`) VALUES ('222', '2222', '2012.01.22');
