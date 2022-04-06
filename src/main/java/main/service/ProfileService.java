a9afc80eea23e2b2c91da28ec2acb53a39e0a4ce
VasilievAV <alva@sespel.com>
2022-04-06 15:35:38 +0300

    Commit

--- src/main/java/main/service/ProfileService.java
+++ src/main/java/main/service/ProfileService.java
@@ -5,6 +5,9 @@ import main.repository.UsersRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
@@ -52,16 +55,8 @@ public class ProfileService {
         } else {
             changeProfileResponse.setResult(true);
 
-            InputStream initialStream = photo.getInputStream();
-            byte[] buffer = new byte[initialStream.available()];
-            initialStream.read(buffer);
-
             String nameImage = randomNameGeneration();
-            File targetFile = new File("src/main/resources/public/imagesProfile/" + nameImage + ".bmp");
-
-            try (OutputStream outStream = new FileOutputStream(targetFile)) {
-                outStream.write(buffer);
-            }
+            writeImageInServer(photo, nameImage);
 
             usersRepository.changeProfile(email, name, password, "http://localhost:8081/imagesProfile/" + nameImage + ".bmp", idUser);
 
@@ -74,6 +69,21 @@ public class ProfileService {
     public String randomNameGeneration() {
         String uuid = UUID.randomUUID().toString();
         return uuid;
+
+    }
+
+    public void writeImageInServer(MultipartFile photo, String nameImage) throws IOException {
+        InputStream initialStream = photo.getInputStream();
+        byte[] buffer = new byte[initialStream.available()];
+        initialStream.read(buffer);
+
+        ByteArrayInputStream bais = new ByteArrayInputStream(buffer);
+        BufferedImage imageBuffer = ImageIO.read(bais);
+        Image resultingImage = imageBuffer.getScaledInstance(36, 36, Image.SCALE_DEFAULT);
+        BufferedImage outputImage = new BufferedImage(36, 36, BufferedImage.TYPE_INT_RGB);
+        outputImage.getGraphics().drawImage(resultingImage, 0, 0, null);
+
+        ImageIO.write(outputImage, "bmp", new File("src/main/resources/public/imagesProfile/" + nameImage + ".bmp"));
 
     }
 
