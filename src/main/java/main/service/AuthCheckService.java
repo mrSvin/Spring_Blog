9a309e21c6a8ee98e8de0ea75558134ecc8dbf86
vasilievav <vipal921@gmail.com>
2025-03-28 10:01:05 +0300

    update

--- src/main/java/main/service/AuthCheckService.java
+++ src/main/java/main/service/AuthCheckService.java
@@ -3,6 +3,7 @@ package main.service;
 import main.api.response.AuthCheckResponse;
 import main.api.response.UserAuthResponse;
 import main.model.User;
+import main.repository.PostRepository;
 import main.repository.UsersRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +11,11 @@ import org.springframework.stereotype.Service;
 public class AuthCheckService {
 
     private final UsersRepository usersRepository;
+    private final PostRepository postRepository;
 
-    public AuthCheckService(UsersRepository usersRepository) {
+    public AuthCheckService(UsersRepository usersRepository, PostRepository postRepository) {
         this.usersRepository = usersRepository;
+        this.postRepository = postRepository;
     }
 
     public AuthCheckResponse getAuthCheck(String authСookie) {
@@ -33,10 +36,13 @@ public class AuthCheckService {
         userAuthResponse.setEmail(userInfo.getEmail());
         if (userInfo.getIs_moderator() == 1) {
             userAuthResponse.setModeration(true);
+            userAuthResponse.setModerationCount(postRepository.findByModerationStatus());
+            userAuthResponse.setSettings(true);
+        } else {
+            userAuthResponse.setModeration(false);
+            userAuthResponse.setSettings(false);
         }
-        userAuthResponse.setModeration(false);
-        userAuthResponse.setModerationCount(1);
-        userAuthResponse.setSettings(true);
+
         authCheckResponse.setUser(userAuthResponse);
         authCheckResponse.setResult(true);
 
